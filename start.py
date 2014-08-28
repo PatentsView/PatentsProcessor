560fbc70dcfe1acc7b6db7cca039548d99b8a764
gregtheyoung <github@twinarchtech.com>
2014-08-28 13:56:58 -0400

    Windows fix for bash

--- start.py
+++ start.py
@@ -140,7 +140,7 @@ def run_clean(process_config):
     if not process_config['clean']:
         return
     doctype = process_config['doctype']
-    command = 'run_clean.sh'
+    command = 'run_clean.bat'
     os.system(command)
 
 def run_consolidate(process_config):
@@ -148,7 +148,7 @@ def run_consolidate(process_config):
         return
     doctype = process_config['doctype']
     # TODO: optionally include previous disambiguation
-    command = 'run_consolidation.sh'
+    command = 'run_consolidation.bat'
     os.system(command)
 
 if __name__=='__main__':
